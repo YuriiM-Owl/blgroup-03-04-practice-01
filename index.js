65b5cd60a9d80d05d09049f5b327d4050c25c0fb
TigranKh21 <tigran.khachaturyants@gmail.com>
2023-09-02 15:20:05 +0300

    task-02

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
  *? інакше відобразити:"Не знаєте? ECMAScript!"
  */
 
-const userAnswer = prompt("Яка офіційна назва JavaScript?");
+/* const userAnswer = prompt("Яка офіційна назва JavaScript?");
 
 const userAnswerToLowerCase = userAnswer.toLocaleLowerCase().trim();
 
@@ -16,3 +16,23 @@ if (userAnswerToLowerCase === "ecmascript") {
 } else {
     alert("Не знаєте? ECMAScript!");
 }
+ */
+
+
+/**
+
+Напишіть програму, яка отримає від користувача
+ число (кількість хвилин) та виведе в консоль
+ рядок у форматі годин та хвилин
+ https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+ 70 === 01:10
+ */
+
+ /* ------------------- Task-02------------------ */
+
+/*  const userUnswer = Number(prompt('Введіть число'));
+ const hours = String(Math.floor(userUnswer / 60)).padStart(2,0);
+ const minutes = String(userUnswer % 60).padStart(2,0);
+ console.log(`${hours}:${minutes}`); */
+
+ /* ------------------- Task-03------------------ */
