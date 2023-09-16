a65e7b66cfa9071d37a7b8b7992fb78872f1d0ad
imlamer2023 <imlamer888@gmail.com>
2023-09-16 17:10:08 +0300

    task -5

--- index.js
+++ index.js
@@ -213,4 +213,30 @@ console.log(totalSum(someObj)); */
 //   }
 //   console.log(updateFruit(fruits));
 
-  /* ------------------- Task-02-04 ------------------ */
+  /* ------------------- Task-02-05 ------------------ */
+//TODO:==============================================
+/**
+*? Поверніть об'єкт, в якому вказано кількість тегів.
+*? Очікуваний результат {js: 3, nodejs: 3, html: 2, css: 2, react: 2}
+
+ */
+const tweets = [
+  { id: "000", likes: 5, tags: ["js", "nodejs"] },
+  { id: "001", likes: 2, tags: ["html", "css"] },
+  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+  { id: "003", likes: 8, tags: ["css", "react"] },
+  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+];
+
+function getObject(arr) {
+  const newArray = arr.flatMap(elem => elem.tags);
+  
+  return newArray.reduce((acc, elem) => {
+    
+    return { ...acc, [elem]: acc[elem]? acc[elem] +1 : 1};
+  } , {}
+) 
+
+}
+console.log(getObject(tweets));
+
